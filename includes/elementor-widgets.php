82362634cad0946b14517fe5070d7cac834044f7
Saqib-wpbrigade <saqib.wpbrigade@gmail.com>
2023-01-05 16:40:36 +0500

    basic files added

---
+++ includes/elementor-widgets.php
@@ -0,0 +1,300 @@
+<?php
+/**
+ * Custom Elementor Repeater Extension.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Main Plus Alliance Elementor Widgets
+ *
+ * The main class that initiates and runs the plugin.
+ *
+ * @since 1.0.0
+ */
+final class Maricopa_Elementor_Widgets {
+
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * @var Maricopa_Elementor_Widgets The single instance of the class.
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of the class is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 * @static
+	 *
+	 * @return Maricopa_Elementor_Widgets An instance of the class.
+	 */
+	public static function instance() {
+
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function __construct() {
+		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ) );
+	}
+
+	/**
+	 * Load Textdomain
+	 *
+	 * Load plugin localization files.
+	 *
+	 * Fired by `init` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function i18n() {
+
+		load_plugin_textdomain( 'maricopa' );
+	}
+
+	/**
+	 * On Plugins Loaded
+	 *
+	 * Checks if Elementor has loaded, and performs some compatibility checks.
+	 * If All checks pass, inits the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function on_plugins_loaded() {
+
+		if ( $this->is_compatible() ) {
+			add_action( 'elementor/init', array( $this, 'init' ) );
+		}
+
+	}
+
+	/**
+	 * Compatibility Checks
+	 *
+	 * Checks if the installed version of Elementor meets the plugin's minimum requirement.
+	 * Checks if the installed PHP version meets the plugin's minimum requirement.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function is_compatible() {
+
+		// Check if Elementor installed and activated.
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_missing_main_plugin' ) );
+			return false;
+		}
+
+		// Check for required Elementor version.
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_elementor_version' ) );
+			return false;
+		}
+
+		// Check for required PHP version.
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_php_version' ) );
+			return false;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Load the plugin only after Elementor (and other plugins) are loaded.
+	 * Load the files required to run the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init() {
+
+		$this->i18n();
+
+		// Add Plugin actions.
+		add_action( 'elementor/widgets/widgets_registered', array( $this, 'init_widgets' ) );
+
+		// Add Category.
+		add_action( 'elementor/elements/categories_registered', array( $this, 'add_elementor_widget_categories' ) );
+
+	}
+
+	/**
+	 * Include widgets register them.
+	 *
+	 * @param Object $elements_manager Elementor widgets manager.
+	 * @return void
+	 */
+	public function add_elementor_widget_categories( $elements_manager ) {
+
+		$elements_manager->add_category(
+			'maricopa_widgets',
+			array(
+				'title' => __( 'Maricopa Widgets', 'maricopa' ),
+				'icon'  => 'fa fa-sliders',
+			)
+		);
+	}
+
+	/**
+	 * Init Widgets
+	 *
+	 * Include widgets files and register them
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init_widgets() {
+
+		// Include Widget files.
+		// require_once __DIR__ . '/elementor-widgets/maricopa-zigzag-timeline.php';
+		// require_once __DIR__ . '/elementor-widgets/maricopa-donor-stories.php';
+		// require_once __DIR__ . '/elementor-widgets/maricopa-foundation-list.php';
+		// require_once __DIR__ . '/elementor-widgets/mccf-student-stories.php';
+		// require_once __DIR__ . '/elementor-widgets/maricopa-news-stories.php';
+	
+		// Register widgets.
+		// \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Maricopa_ZigZag_Timeline() );
+		// \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Maricopa_Donor_Stories() );
+		// \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Maricopa_Foundation_List() );
+		// \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Maricopa_News_Stories() );
+		// \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Maricopa_Student_Stories() );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have Elementor installed or activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_missing_main_plugin() {
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'maricopa' ),
+			'<strong>' . esc_html__( 'Elementor Extension', 'maricopa' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'maricopa' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
+
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'maricopa' ),
+			'<strong>' . esc_html__( 'Elementor Extension', 'maricopa' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'maricopa' ) . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'maricopa' ),
+			'<strong>' . esc_html__( 'Elementor Extension', 'maricopa' ) . '</strong>',
+			'<strong>PHP</strong>',
+			self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+}
+
+
+Maricopa_Elementor_Widgets::instance();
---
+++ index-widgets.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name: Maricopa Functionality Plugin
+ * Plugin URI: https://LANETERRALEVER.com
+ * Description: CPT and other API functionalities
+ * Version: 1.0.0
+ * Author: LANETERRALEVER
+ * Author URI: https://LANETERRALEVER.com
+ * Text Domain: maricopa
+ *
+ * @package maricopa
+ */
+
+if ( ! defined( 'MARICOPA_PLUGIN_VERSION' ) ) {
+	define( 'MARICOPA_PLUGIN_VERSION', '1.0.0' );
+}
+
+if ( ! defined( 'MARICOPA_PLUGIN_DIR' ) ) {
+	define( 'MARICOPA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'MARICOPA_PLUGIN_INC_DIR' ) ) {
+	define( 'MARICOPA_PLUGIN_INC_DIR', MARICOPA_PLUGIN_DIR . '/includes/' );
+}
+
+if ( ! defined( 'MARICOPA_PLUGIN_URL' ) ) {
+	define( 'MARICOPA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/**
+ * Create object for Maricopa Functionality Plugin for calling the functionality files.
+ *
+ * @since 1.0.0
+ */
+class Maricopa_Functionality_Plugin {
+
+	/**
+	 * Constructor of the Class.
+	 */
+	public function __construct() {
+
+		// // CTP definitions.
+		include_once MARICOPA_PLUGIN_INC_DIR . 'elementor-widgets.php';
+		// include_once MARICOPA_PLUGIN_INC_DIR . 'custom-post-types.php';
+		// include_once MARICOPA_PLUGIN_DIR . '/featured-meta.php';
+	}
+
+}
+
+// plugin init.
+$maricopa_functionality_plugin = new Maricopa_Functionality_Plugin();
